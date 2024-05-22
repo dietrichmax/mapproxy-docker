df892c5f54b825d41143fe7e31ec4dcf364ab306
Max Dietrich <mail@mxd.codes>
2024-05-22 16:53:18 +0200

    cache

--- start.sh
+++ start.sh
@@ -6,7 +6,6 @@ cd /mapproxy
 
 groupadd mapproxy && \
 useradd --home-dir /mapproxy -s /bin/bash -g mapproxy mapproxy && \
-chown -R mapproxy:mapproxy /mapproxy/cache_data
 
 # create config files if they do not exist yet
 if [ ! -f /mapproxy/config/mapproxy.yaml ]; then
