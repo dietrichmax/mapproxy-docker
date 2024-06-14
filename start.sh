9875dfd12a755cd3f3cbb723f40cc9af475f6fff
Max Dietrich <mail@mxd.codes>
2024-06-14 13:57:33 +0200

    test

--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+done=0
+trap 'done=1' TERM INT
 cd /mapproxy
 
 # Add group
@@ -13,7 +15,6 @@ echo "Adding mapproxy user"
 if ! id -u mapproxy >/dev/null 2>&1; then
   useradd --home-dir /mapproxy -s /bin/bash -g mapproxy mapproxy
 fi
-
 
 # create config files if they do not exist yet
 echo "Checking if config file exists"
@@ -32,3 +33,8 @@ service nginx restart -g 'daemon off;'
 
 echo "Starting Mapproxy"
 su mapproxy -c "/usr/local/bin/uwsgi --ini /mapproxy/uwsgi.conf &"
+
+while [ $done = 0 ]; do
+  sleep 1 &
+  wait
+done
