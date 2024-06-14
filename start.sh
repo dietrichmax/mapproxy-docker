6469ee6c57719a6988aac6aaaa5b4543da5cb5e0
Max Dietrich <mail@mxd.codes>
2024-06-14 13:53:37 +0200

    test

--- start.sh
+++ start.sh
@@ -1,32 +1,34 @@
 #!/bin/bash
 
-USER_NAME=${USER:-mapproxy}
-USER_ID=${MAPPROXY_USER_ID:-1003}
-GROUP_NAME=${GROUP_NAME:-mapproxy}
-GROUP_ID=${MAPPROXY_GROUP_ID:-1003}
-
 cd /mapproxy
 
 # Add group
+echo "Adding mapproxy group"
 if [ ! $(getent group mapproxy) ]; then
   groupadd mapproxy
 fi
 
 # Add user to system
+echo "Adding mapproxy user"
 if ! id -u mapproxy >/dev/null 2>&1; then
   useradd --home-dir /mapproxy -s /bin/bash -g mapproxy mapproxy
 fi
 
+
 # create config files if they do not exist yet
+echo "Checking if config file exists"
 if [ ! -f /mapproxy/config/mapproxy.yaml ]; then
   echo "No mapproxy configuration found. Creating one from template."
   mapproxy-util create -t base-config config
 fi
 
 # fix permissions
+echo "Fixing permissions"
 chown -R mapproxy:mapproxy /mapproxy/
 
 # start mapproxy and nginx
+echo "Restarting nginx"
 service nginx restart -g 'daemon off;' 
+
+echo "Starting Mapproxy"
 su mapproxy -c "/usr/local/bin/uwsgi --ini /mapproxy/uwsgi.conf &"
-echo "Started Mapproxy"
