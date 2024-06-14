6917634887f267dba2f941952ff1362195cab55d
Max Dietrich <mail@mxd.codes>
2024-06-14 10:43:15 +0200

    env test

--- entrypoint.sh
+++ entrypoint.sh
@@ -22,7 +22,7 @@ if ! id -u "${USER_NAME}" >/dev/null 2>&1; then
 fi
 
 # create config files if they do not exist yet
-if [ ! -f /mapproxy/config/*.yaml ]; then
+if [ ! -f /mapproxy/config/mapproxy.yaml ]; then
   echo "No mapproxy configuration found. Creating one from template."
   mapproxy-util create -t base-config config
 fi
