9c24ce9370328aa66b477eccc7cb48b3dfc01eeb
Max Dietrich <mail@mxd.codes>
2024-06-14 10:27:26 +0200

    env test

--- entrypoint.sh
+++ entrypoint.sh
@@ -21,9 +21,8 @@ if ! id -u "${USER_NAME}" >/dev/null 2>&1; then
   useradd -l -m -d /home/"${USER_NAME}"/ -u "${USER_ID}" --gid "${GROUP_ID}" -s /bin/bash -G "${GROUP_NAME}" "${USER_NAME}"
 fi
 
-
 # create config files if they do not exist yet
-if [ ! -f /mapproxy/config/mapproxy.yaml ]; then
+if [ ! -f /mapproxy/config/*.yaml ]; then
   echo "No mapproxy configuration found. Creating one from template."
   mapproxy-util create -t base-config config
 fi
@@ -32,4 +31,5 @@ fi
 chown -R mapproxy:mapproxy /mapproxy/
 
 # start mapproxy and nginx
-su mapproxy -c "/usr/local/bin/uwsgi --ini /mapproxy/uwsgi.conf && /usr/sbin/nginx -g 'daemon off;'"
+service nginx restart -g 'daemon off;' &&
+su mapproxy -c "/usr/local/bin/uwsgi --ini /mapproxy/uwsgi.conf
