c00a54b2e9ccbef618e1ed10c3a1c1a3fee96ef0
Max Dietrich <40794289+dietrichmax@users.noreply.github.com>
2024-07-04 16:05:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@
 # fileConfig(r'/mapproxy/config/log.ini', {'here': os.path.dirname(__file__)})
 
 from mapproxy.multiapp import make_wsgi_app
-application = make_wsgi_app("/mapproxy/config/", allow_listing=True)
+application = make_wsgi_app("/mapproxy/config/", allow_listing=True, reloader=True)
