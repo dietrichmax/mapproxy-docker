7ebcd82489f35a8b1b1388ebdbcb54980cbc56aa
Max Dietrich <mail@mxd.codes>
2024-12-09 09:39:24 +0100

    permissions

--- start.sh
+++ start.sh
@@ -5,15 +5,18 @@ trap 'done=1' TERM INT
 cd /mapproxy
 
 service ssh start
+echo "Started ssh service"
 
 # Add group
 if [ ! $(getent group mapproxy) ]; then
   groupadd mapproxy
+  echo "Added group mapproxy"
 fi
 
 # Add user to system
 if ! id -u mapproxy >/dev/null 2>&1; then
   useradd --home-dir /mapproxy -s /bin/bash -g mapproxy mapproxy
+  echo "Added user mapproxy"
 fi
 
 # create config files if they do not exist yet
@@ -23,11 +26,14 @@ if [ ! -f /mapproxy/config/mapproxy.yaml ]; then
 fi
 
 # fix permissions
-chown -R mapproxy:mapproxy /mapproxy/ /mapproxy/config/ /mapproxy/cache_data/
+#chown -R mapproxy:mapproxy /mapproxy/ /mapproxy/config/ /mapproxy/cache_data/
+#echo "Fixed permissions"
 
-# start mapproxy and nginx
+# start nginx
 service nginx restart -g 'daemon off;' 
+echo "Started Nginx"
 
+# start mapproxy
 su mapproxy -c "/usr/local/bin/uwsgi --ini /mapproxy/uwsgi.conf &"
 echo "Started Mapproxy"
 
